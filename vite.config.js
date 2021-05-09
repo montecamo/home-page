8588ce14f2e95fbf40dd4edba07b40f38b02953c
montecamo <merrychristek@gmail.com>
2021-05-09 13:59:09 +0300

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,4 @@ import svelte from "@sveltejs/vite-plugin-svelte";
 
 export default defineConfig({
   plugins: [svelte()],
-  optimizeDeps: {
-    exclude: ["@fortawesome/fontawesome-free"],
-  },
 });
